49429387b870e954e9a36e69ed82acf33eda8c44
Walid Lezzar <walid.lezzar@schibsted.com>
2020-06-22 10:58:09 +0200

    Bump to v0.23.1

--- Formula/zoe.rb
+++ Formula/zoe.rb
@@ -5,10 +5,10 @@ class Zoe < Formula
   desc "The missing companion for Kafka"
   homepage "https://adevinta.github.io/zoe"
   bottle :unneeded
-  version "0.23.0"
+  version "0.23.1"
   
-  url "https://github.com/adevinta/zoe/releases/download/v0.23.0/zoe-0.23.0.zip"
-  sha256 "ef4537143275cde3d080edf887953f1891b427a839210382900d9b4c6899d3e3"
+  url "https://github.com/adevinta/zoe/releases/download/v0.23.1/zoe-0.23.1.zip"
+  sha256 "cd24d25ab4c2a2a0731b5dbdfb2a641df1235b84977860b953534bfcde32b170"
 
   def install
     bin.install "bin/zoe"
