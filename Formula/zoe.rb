8d932d380df432913e07056ee37ffffe6580ab3d
Walid Lezzar <walid.lezzar@schibsted.com>
2020-08-07 16:03:03 +0200

    Bump to v0.26.0

--- Formula/zoe.rb
+++ Formula/zoe.rb
@@ -1,14 +1,11 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Zoe < Formula
   desc "The missing companion for Kafka"
   homepage "https://adevinta.github.io/zoe"
   bottle :unneeded
-  version "0.24.0"
+  version "0.26.0"
   
-  url "https://github.com/adevinta/zoe/releases/download/v0.24.0/zoe-0.24.0.zip"
-  sha256 "0f304df30a12498e1b6cf98371150b86bec372badae7e8a9c5da6fbdea33975b"
+  url "https://github.com/adevinta/zoe/releases/download/v0.26.0/zoe-0.26.0.zip"
+  sha256 "6386f6821119641e1e193e2dcd792806e0c9e68ee70159f5c65f4ae65fc082f4"
 
   def install
     bin.install "bin/zoe"
