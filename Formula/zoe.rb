7f533b174d2380dc87a5074d3e3c0201f16db439
wlezzar <wlezzar@users.noreply.github.com>
2021-06-22 08:31:31 +0000

    [auto] Update brew formula

--- Formula/zoe.rb
+++ Formula/zoe.rb
@@ -2,10 +2,10 @@ class Zoe < Formula
   desc "The kafka CLI for humans"
   homepage "https://adevinta.github.io/zoe"
   bottle :unneeded
-  version "0.27.1"
+  version "0.27.2"
   
-  url "https://github.com/adevinta/zoe/releases/download/v0.27.1/zoe-0.27.1.zip"
-  sha256 "d21513473f914a3c213566e12b8c4e092b428968e357a7d31c1029718f62c9fa"
+  url "https://github.com/adevinta/zoe/releases/download/v0.27.2/zoe-0.27.2.zip"
+  sha256 "90f86120a9bba1bf5eb0d105b7f865d1f6af9de1447adbf25f81bb0729080c17"
 
   def install
     bin.install "bin/zoe"
