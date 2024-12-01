1a1a73b53ce42b0f313056340c83d3fc0081d696
LouisVN <lvn.vanniekerk@gmail.com>
2024-12-01 21:17:27 +0100

    chore(version): update to 0.30.0

--- Formula/zoe.rb
+++ Formula/zoe.rb
@@ -1,10 +1,10 @@
 class Zoe < Formula
   desc "The kafka CLI for humans"
   homepage "https://adevinta.github.io/zoe"
-  version "0.29.0"
+  version "0.30.0"
   
-  url "https://github.com/adevinta/zoe/releases/download/v0.29.0/zoe-0.29.0.zip"
-  sha256 "4b42901a24aa3a37582885e4898af731eb201007db7b2eca8feb61b013d2b82b"
+  url "https://github.com/adevinta/zoe/releases/download/v0.30.0/zoe-0.30.0.zip"
+  sha256 "d20880638fd8893c45885df0d49e36ecc08b0880f8c3871e4219bd64f029bbf4"
 
   def install
     bin.install "bin/zoe"
